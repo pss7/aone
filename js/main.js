23236d86f4a4bd5934a91fdff78c026f10e37daf
parksangseok <park0157@naver.com>
2025-09-16 16:40:54 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -4,9 +4,43 @@ $(function () {
   //aos 초기화
   $(window).load(function () {
     $("#mainTitleWrap").addClass("active");
+    $("#mainTitleWrap .mainTitle").addClass("active");
     AOS.init({
       duration: 2000
     });
+  });
+
+  //메인
+  $('#mainTitleWrap .slick').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $('#mainTitleWrap .mainTitle').removeClass('active');
+    $('#mainTitleWrap .progressbarBox .dots').removeClass('active');
+  });
+  $('#mainTitleWrap .slick').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    $('#mainTitleWrap .mainTitle').addClass('active');
+    $('#mainTitleWrap .progressbarBox .dots').addClass('active');
+  });
+
+  $('#mainTitleWrap .slick').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $('#mainTitleWrap .progressbarBox .count').html('<em class="current">' + i + '</em> <em class="total">' + slick.slideCount + '</em>');
+    $('#mainTitleWrap .progressbarBox .dots').addClass('active');
+  });
+
+  $("#mainTitleWrap .slick").slick({
+    autoplay: false,
+    arrows: true,
+    dots: false,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 5000,
+    speed: 2000,
+    prevArrow: $('#mainTitleWrap .controlBox .prev'),
+    nextArrow: $('#mainTitleWrap .controlBox .next'),
   });
 
   //후기 영상
@@ -120,9 +154,9 @@ $(function () {
     nextArrow: $('#infoWrap03 .next'),
   });
 
- $('#infoWrap03 .box').on('click keydown', function(e){
+  $('#infoWrap03 .box').on('click keydown', function (e) {
     // 키보드 접근성: Enter(13), Space(32)
-    if(e.type === 'keydown' && e.which !== 13 && e.which !== 32){
+    if (e.type === 'keydown' && e.which !== 13 && e.which !== 32) {
       return;
     }
     e.preventDefault();
